c2d211e9ce405e05c5eed2a346fe3a30e28ce9b5
godfirstfamilymoney-boop <godfirstfamilymoney@gmail.com>
2026-01-27 20:49:26 -0500

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD88ZxhCOxPs1IFzITYAGO0gpIyp4tLzr8",
+  authDomain: "my-way-clt-2026.firebaseapp.com",
+  projectId: "my-way-clt-2026",
+};
+
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore();
